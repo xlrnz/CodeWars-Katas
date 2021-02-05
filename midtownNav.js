226b5930e60f3524956b047fd4bfda24278487e4
xlrnz <xlrnz@protonmail.com>
2021-02-05 19:26:48 +0100

    MidtownNav Katas Added!

---
+++ midtownNav.js
@@ -0,0 +1,39 @@
+function midtownNav(start, end) {
+  // Use regex to get the digit inside start and end.
+  // First number is for the avenue second for the street
+  const coordinate = /\d{1,2}/g;
+
+  // Create two object for street and avenue
+  let avenue = {
+    start: start.match(coordinate)[0],
+    end: end.match(coordinate)[0],
+    instructions: { dir: "", n_blocks: 0 },
+  };
+
+  let street = {
+    start: start.match(coordinate)[1],
+    end: end.match(coordinate)[1],
+    instructions: { dir: "", n_blocks: 0 },
+  };
+
+  // Get the parameters and calculate instructions for each object
+  const getInstruction = () => {
+    avenue.instructions.dir = avenue.start <= avenue.end ? "west" : "east";
+    avenue.instructions.n_blocks =
+      avenue.start < avenue.end
+        ? avenue.end - avenue.start
+        : avenue.start - avenue.end;
+
+    street.instructions.dir = street.start <= street.end ? "north" : "south";
+    street.instructions.n_blocks =
+      street.start < street.end
+        ? street.end - street.start
+        : street.start - street.end;
+  };
+
+  getInstruction();
+  return `Walk ${street.instructions.n_blocks} blocks ${street.instructions.dir}, and ${avenue.instructions.n_blocks} blocks ${avenue.instructions.dir}`;
+}
+
+// For testing purpose
+module.exports = midtownNav;
